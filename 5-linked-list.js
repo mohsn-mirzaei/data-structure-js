5cabe29b895b7e2c1883a113da93fff3c65de651
Mohsen <mohsn.mirzaei@gmail.com>
2024-03-23 18:26:28 +0330

    Set Up Linked List

---
+++ 5-linked-list.js
@@ -0,0 +1,19 @@
+// 1-
+// Instead of index, we have value
+// And each value has a pointer to the next element
+// So our list consists of data where each value points to the next value
+
+// 2-
+// To add data to the linked-list, this insertion is always done from the beginning or the end of the list for us.
+// So it is useful in performance.
+
+// When creating a custom data structure, using functions alone is not practical,
+// because they undergo a specific process. Therefore, we need to use classes instead.
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+
+const linkedList = new LinkedList();
